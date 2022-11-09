e182af21d4fb8a463e85b48892457bad94468e5c
1zach <zachjamesthomas@gmail.com>
2022-11-09 22:25:23 +0900

    Completed all challenges

--- myApp.js
+++ myApp.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+const bodyParser = require('body-parser')
+
 let express = require('express');
 let app = express();
 
@@ -14,9 +17,47 @@ let staticPath = __dirname + "/public"
 app.use("/public", express.static(staticPath))
 
 
-app.get("/json", function(req, res) {
-    res.json({"message": "hello json"})}
-    )
+// app.get("/json", function(req, res) {
+//     if (process.env.MESSAGE_STYLE == "uppercase") {
+//         res.json({"message": "HELLO JSON"})
+//     }
+//     else {
+//         res.json({"message":"hello json"})
+//     }}
+//     )
+app.use("/json", function(req,res,next) {
+    console.log(req.method, req.path, " - ",req.ip)
+    next()
+})
+
+app.get("/now", function(req, res, next) {
+    req.time = new Date().toString()
+    next()
+}, function(req, res) {
+    res.json({"time": req.time})
+})
+
+app.get("/:word/echo", function(req, res) {
+    const {word} = req.params
+        res.json({echo: word})
+    })
+
+// app.get("/name", function(req, res) {
+//     const {first, last} = req.query
+//     res.json({name: first, last})
+// }
+// )
+
+app.use(
+    bodyParser.urlencoded({extended: false})
+)
+app.use(bodyParser.json())
+
+app.use("/name", function(req,res) {
+    const {first, last} = req.body
+    res.json({name: first, last})
+})
+
 
 
 
