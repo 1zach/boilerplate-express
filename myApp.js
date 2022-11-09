334c7f667f7d4673372d6bf71d0c25a1173d7afc
1zach <zachjamesthomas@gmail.com>
2022-11-09 21:38:44 +0900

    Added json

--- myApp.js
+++ myApp.js
@@ -1,13 +1,22 @@
 let express = require('express');
 let app = express();
 
+let absolutePath = __dirname + "/views/index.html"
+
 console.log("Hello World")
 
 app.get("/", function(req, res) {
-    res.send("Hello Express")
+    res.sendFile(absolutePath)
 });
 
+let staticPath = __dirname + "/public"
 
+app.use("/public", express.static(staticPath))
+
+
+app.get("/json", function(req, res) {
+    res.json({"message": "hello json"})}
+    )
 
 
 
